3ff4d3cfcfde52fb7017084ef647a994891b7848
Манченко Иван <hhypest@gmail.com>
2026-08-10 19:16:39 +0300

    Add Infrastructure dependency registration

---
+++ src/TestApp.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using TestApp.Application.Abstractions;
+using TestApp.Infrastructure.Identity;
+using TestApp.Infrastructure.Persistence;
+
+namespace TestApp.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, Action<DbContextOptionsBuilder> configureDb)
+    {
+        services.AddDbContext<AppDbContext>(configureDb);
+        services.AddHttpContextAccessor();
+        services.AddScoped<ITestRepository, TestRepository>();
+        services.AddScoped<IPublishedTestRevisionRepository, PublishedTestRevisionRepository>();
+        services.AddScoped<ITestAssignmentRepository, TestAssignmentRepository>();
+        services.AddScoped<ITestAttemptRepository, TestAttemptRepository>();
+        services.AddScoped<IUnitOfWork>(sp => sp.GetRequiredService<AppDbContext>());
+        services.AddScoped<ICurrentActor, HttpCurrentActor>();
+        services.AddSingleton<IClock, SystemClock>();
+        return services;
+    }
+}
