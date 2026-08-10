936a54cc17777529bd8a78160056402061fb9095
Манченко Иван <hhypest@gmail.com>
2026-08-10 20:01:46 +0300

    Add Minimal API host integration test

---
+++ tests/TestApp.IntegrationTests/ApiHostTests.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace TestApp.IntegrationTests;
+
+public sealed class ApiHostTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _factory;
+
+    public ApiHostTests(WebApplicationFactory<Program> factory)
+    {
+        _factory = factory.WithWebHostBuilder(builder =>
+        {
+            builder.UseSetting("Keycloak:Authority", "https://identity.invalid/realms/testapp");
+            builder.UseSetting("Keycloak:Audience", "testapp-api");
+            builder.UseSetting("ConnectionStrings:Database", "Data Source=:memory:");
+        });
+    }
+
+    [Fact]
+    public async Task Protected_endpoint_rejects_anonymous_request()
+    {
+        using var client = _factory.CreateClient();
+
+        using var response = await client.GetAsync("/api/me/assignments", TestContext.Current.CancellationToken);
+
+        Assert.Equal(System.Net.HttpStatusCode.Unauthorized, response.StatusCode);
+    }
+}
