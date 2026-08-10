01e3dbe1634d9323ae6e0dfdcf5839ccea6db9a2
Манченко Иван <hhypest@gmail.com>
2026-08-10 19:09:39 +0300

    Support persistence materialization in aggregate roots

--- src/TestApp.Domain/Entities/AggregateRoot.cs
+++ src/TestApp.Domain/Entities/AggregateRoot.cs
@@ -2,14 +2,12 @@ using TestApp.Domain.Events;
 
 namespace TestApp.Domain.Entities;
 
-public abstract class AggregateRoot<TId> : Entity<TId>
-    where TId : notnull
+public abstract class AggregateRoot<TId> : Entity<TId> where TId : notnull
 {
     private readonly List<IDomainEvent> _domainEvents = [];
 
-    protected AggregateRoot(TId id) : base(id)
-    {
-    }
+    protected AggregateRoot() { }
+    protected AggregateRoot(TId id) : base(id) { }
 
     public IReadOnlyCollection<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
 
