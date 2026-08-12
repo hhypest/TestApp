15f64fb7cf92a66675b5fe39c10ab99dc7176a84
Манченко Иван <hhypest@gmail.com>
2026-08-12 19:58:19 +0300

    Fix Idempotency-Key E2E namespace

--- tests/TestApp.IntegrationTests/IdempotencyHttpContractTests.cs
+++ tests/TestApp.IntegrationTests/IdempotencyHttpContractTests.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
+using TestApp.Api;
 using TestApp.Domain.Revisions;
 using TestApp.Domain.Tests;
 using Xunit;
