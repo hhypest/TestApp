f9e6150d61b3a8e4babf98fa83c92c0c832a646e
Манченко Иван <hhypest@gmail.com>
2026-08-12 13:28:05 +0300

    Add application concurrency conflict exception

---
+++ src/TestApp.Application/Common/ConcurrencyConflictException.cs
@@ -0,0 +1,7 @@
+namespace TestApp.Application.Common;
+
+public sealed class ConcurrencyConflictException : Exception
+{
+    public ConcurrencyConflictException(string message, Exception? innerException = null)
+        : base(message, innerException) { }
+}
