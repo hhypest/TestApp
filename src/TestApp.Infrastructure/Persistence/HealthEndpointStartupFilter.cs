45440afab73c8f043865e64abfbe0eb0bd759d91
Манченко Иван <hhypest@gmail.com>
2026-08-12 14:42:58 +0300

    Fix health endpoint status code references

--- src/TestApp.Infrastructure/Persistence/HealthEndpointStartupFilter.cs
+++ src/TestApp.Infrastructure/Persistence/HealthEndpointStartupFilter.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 namespace TestApp.Infrastructure.Persistence;
